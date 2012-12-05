6211d78cec0d0f1a05c5fb0adb534218e572152f
Aidan Feldman <aidan.feldman@gmail.com>
2012-12-05 18:23:02 -0500

    add automated test for stylesheet compilation, and add build to Travis

---
+++ test/stylesheets_test.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/kilter'
+
+require 'bundler'
+Bundler.require(:default, :development, :test)
+
+require 'test/unit'
+require 'sass/plugin'
+require 'compass/test_case'
+
+
+# modified from http://compass-style.org/help/tutorials/testing/
+class StylesheetsTest < Compass::TestCase
+  def test_stylesheets
+    my_sass_files.each do |sass_file|
+      assert_compiles(sass_file) do |result|
+        assert(!result.nil?, "#{sass_file} should not be nil.")
+        assert(!result.empty?, "#{sass_file} should not be an empty string.")
+      end
+    end
+  end
+
+  protected
+  def my_sass_files
+    Dir.glob(File.expand_path(File.join(File.dirname(__FILE__), '..', './**/[^_]*.{sass,scss}')))
+  end
+end
